64df79f07a59d5f38fd94253ad9e2b27e389992f
Felix Parker <flixpar@FP-MBP.local>
2018-12-08 13:47:25 -0500

    implemented f-score loss

--- models/loss.py
+++ models/loss.py
@@ -1,15 +1,6 @@
 import torch
 from torch import nn
 
-class FocalLoss(nn.Module):
-
-	def __init__(self, weight=None, reduction='elementwise_mean', n_classes=28):
-		super(FocalLoss, self).__init__()
-		raise NotImplementedError("Multi-class focal loss not yet implemented")
-
-	def forward(self, x):
-		raise NotImplementedError("Multi-class focal loss not yet implemented")
-
 class BinaryFocalLoss(nn.Module):
 
 	def __init__(self, gamma=2.0, weight=None, reduction="elementwise_mean"):
@@ -63,3 +54,39 @@ class MultiLabelFocalLoss(nn.Module):
 
 		return loss
 
+class FBetaLoss(nn.Module):
+
+	def __init__(self, beta=1, soft=True, weight=None, reduction="elementwise_mean"):
+		super(FBetaLoss, self).__init__()
+		if weight is not None: self.register_buffer('weight', weight)
+		self.weight = weight
+		if reduction != "elementwise_mean": raise NotImplementedError()
+		self.reduction = reduction
+		self.beta = beta
+		self.eps = 1e-7
+		self.soft = soft
+
+	def forward(self, input, target):
+
+		input = torch.sigmoid(input)
+		target = target.float()
+
+		if not self.soft:
+			input = (input >= 0.5).float()
+
+		tp = torch.sum(input * target,             dim=0)
+		tn = torch.sum((1 - input) * (1 - target), dim=0)
+		fp = torch.sum(input * (1 - target),       dim=0)
+		fn = torch.sum((1 - input) * target,       dim=0)
+
+		precision = tp / (tp + fp + self.eps)
+		recall    = tp / (tp + fn + self.eps)
+
+		beta2 = self.beta**2
+		loss = (1 + beta2) * (precision * recall) / ((beta2 * precision) + recall + self.eps)
+
+		if self.weight is not None:
+			loss = torch.mul(loss, self.weight)
+
+		loss = 1 - loss.mean()
+		return loss
