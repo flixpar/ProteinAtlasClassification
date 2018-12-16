c7321474f1a221b12e1eacb9bfd246a3a5095902
Felix Parker <flixpar@FP-MBP.local>
2018-12-16 01:08:34 -0500

    major changes to args

--- args.py
+++ args.py
@@ -8,14 +8,15 @@ class Args:
 	##############################
 
 	epochs = 30                     # DEFAULT 30 (int 1-99)
-	initial_lr = 1e-5               # DEFAULT 2e-5 (float)
+	initial_lr = 1e-5               # DEFAULT 1e-5 (float)
 	batch_size = 16                 # DEFAULT 16 (int)
+	
+	arch = "inceptionv4"            # DEFAULT resnet152 (resnet50 | resnet152 | senet154 | inceptionv4)
 
-	trainval_ratio = 0.90           # DEFAULT 0.9
 	img_size = None                 # DEFAULT None (None | int 224-1024)
 	full_size = False               # DEFAULT False (bool)
+	use_external = False            # DEFAULT False (bool)
 	img_channels = "rgby"           # DEFAULT g (str {r, g, b, y})
-	arch = "inceptionv4"            # DEFAULT resnet152 (resnet50 | resnet152 | senet154 | inceptionv4)
 
 	loss = "softmargin"             # DEFAULT softmargin (softmargin | focal | fbeta)
 	focal_gamma = 2                 # DEFAULT 2 (float)
@@ -28,29 +29,23 @@ class Args:
 	workers = 8                     # DEFAULT 8 (int 0-16)
 
 	log_freq = 5                    # DEFAULT 10 (int)
+	trainval_ratio = 0.90           # DEFAULT 0.9
 	n_val_samples = None            # DEFAULT None (int | None)
 
-	##############################
-	###### Image Transforms ######
-	##############################
-
-	train_transforms = tfms.Compose([
+	train_augmentation = tfms.Compose([
 		tfms.HorizontalFlip(p=0.5),
 		tfms.VerticalFlip(p=0.5),
 		tfms.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=20),
 		tfms.RandomBrightnessContrast(),
-		tfms.GaussNoise(var_limit=(2, 8)),
-		tfms.Normalize(mean=[], std=[])
-	])
-	test_transforms = tfms.Compose([
-		tfms.Normalize(mean=[], std=[])
+		tfms.GaussNoise(var_limit=(2, 8))
 	])
 
 	##############################
 	########### Test #############
 	##############################
 
-	test_augmentation = None        # DEFAULT None (None)
+	test_augmentation = []        # DEFAULT [] (list)
+	test_postprocessing = False   # DEFAULT False (bool)
 
 	##############################
 	########## Paths #############
